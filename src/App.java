573df13a9637d5a6115a95ffd6923bc54dfff622
victorHL99 <victorlessa.dev@gmail.com>
2023-01-09 20:22:12 -0300

    feat: :sparkles: create function for return mutiplication table for the number, and user can insert number with comma

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import javax.swing.JOptionPane;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+  public static void main(String[] args) {
+
+    int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    String baseNumberString = JOptionPane.showInputDialog("Enter a number: ").replace(",", ".");
+
+    Double baseNumber = Double.parseDouble(baseNumberString);
+
+    System.out.println("Tabela de multiplicação de X");
+    for (int i = 0; i <= numbers.length; i++) {
+      System.out.println(baseNumber * i);
     }
+
+  }
 }
